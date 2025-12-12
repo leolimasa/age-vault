584452cfaeb4d5b96e8b34b27f1cb802aaffda35
Leonardo Sá <leo@leo-sa.com>
2025-12-12 15:27:38 -0600

    added binary to flake

--- flake.nix
+++ flake.nix
@@ -16,14 +16,24 @@
           pname = "age-vault";
           version = "0.1.0";
           src = ./.;
-          
+
+          # Specify the main package to build
+          subPackages = [ "cmd/age-vault" ];
+
           vendorHash = "sha256-RLRvYay5nuTWSAN0qvSpgzkZhDHNGMi49m3JwtUNw0s=";
 
           meta = with pkgs.lib; {
             description = "Share secrets across machines. Built on top of the `age` encryption tool.";
             homepage = "https://github.com/leolimasa/age-vault";
-            license = licenses.mit;
+            license = licenses.bsd3;
+            mainProgram = "age-vault";
           };
+        };
+
+        # Make it easy to run with 'nix run'
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/age-vault";
         };
 
         devShells.default = pkgs.mkShell {
